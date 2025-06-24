383590616ed059151073ab029519e8f288d1fcec
이상진 <102288397+lsj0202@users.noreply.github.com>
2025-06-24 14:06:28 +0900

    chore: metadata 상세하게 작성, 공통 테마 추가, 폰트 설정 (#2)
    * chore: metadata 상세하게 작성, 공통 테마 추가, 폰트 설정
    
    * chore: ci 수정

---
+++ src/constants/metadata.ts
@@ -0,0 +1,6 @@
+export const META_DATA = {
+  title: 'ResumeLink',
+  icons: '/images/favicon.svg',
+  description: '작성한 자기소개와 프로젝트를 바탕으로 경쟁력있는 이력서를 만들어 보세요!',
+  keyword: ['이력서', '커피챗', '프로젝트', '개발자', '커리어'],
+};
