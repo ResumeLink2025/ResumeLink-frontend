af46372988fc184d256da60d04c9b95ee266ad61
lsj0202 <w1004123@gmail.com>
2025-06-24 22:37:02 +0900

    chore: 프롬프트 내용을 영어로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-
 export default function Home() {
-  const [contents, setContents] = useState('기본 내용');
-
-  useEffect(() => {
-    setContents('내용 추가함');
-  }, []);
-
-  console.log('contents', contents);
-
   return <></>;
 }
