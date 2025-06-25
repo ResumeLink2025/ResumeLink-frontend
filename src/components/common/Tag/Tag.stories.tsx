b0fbb7f83eb71123e79a5e681008cdb1c30487dc
lsj0202 <w1004123@gmail.com>
2025-06-25 22:42:48 +0900

    feat: tag 컴포넌트 개발 및 스토리북 작성

---
+++ src/components/common/Tag/Tag.stories.tsx
@@ -0,0 +1,63 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+import { useState } from 'react';
+
+import Tag from '.';
+
+type TagType = typeof Tag;
+
+const meta: Meta<typeof Tag> = {
+  title: 'common/Tag',
+  component: Tag,
+  argTypes: {
+    size: {
+      control: 'inline-radio',
+      options: ['small', 'medium', 'large'],
+    },
+    styleType: {
+      control: 'inline-radio',
+      options: ['primary', 'gray', 'outline', 'outlinePrimary'],
+    },
+    isSelected: {
+      control: 'boolean',
+    },
+  },
+};
+
+export default meta;
+
+export const DefaultTag: StoryObj<TagType> = {
+  render: (args) => {
+    const tags = ['React', 'Next', 'Express', 'Nest', 'Swift'];
+
+    const [selectedTags, setSelectedTags] = useState<string[]>();
+
+    const onClickSelectTag = (tag: string) => {
+      setSelectedTags((prevState = []) => {
+        const isSelected = prevState?.includes(tag);
+
+        if (isSelected) {
+          return prevState.filter((prevTag) => tag !== prevTag);
+        } else {
+          return [...prevState, tag];
+        }
+      });
+    };
+
+    console.log('selectedTags', selectedTags);
+
+    return (
+      <div className="flex gap-2">
+        {tags.map((tag) => (
+          <Tag
+            key={tag}
+            isSelected={selectedTags?.includes(tag)}
+            onClick={() => onClickSelectTag(tag)}
+            {...args}
+          >
+            {tag}
+          </Tag>
+        ))}
+      </div>
+    );
+  },
+};
