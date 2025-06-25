4b6e9bffa8175ee0feaef5f8eb6471c40e49f78d
seoku9 <sukkoo777@gmail.com>
2025-06-25 19:06:03 +0900

    feat: textArea  컴포넌트 수정완료

--- src/components/common/textArea/index.tsx
+++ src/components/common/textArea/index.tsx
@@ -1,6 +1,8 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 
+import { cn } from '@/utils/styleMerge';
+
 interface TextAreaProps {
   value?: string;
   placeholder?: string;
@@ -29,6 +31,7 @@ const TextArea: React.FC<TextAreaProps> = ({
   }, [value]);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (disabled) return;
     setInternalValue(e.target.value);
     onChange?.(e.target.value);
   };
@@ -39,7 +42,14 @@ const TextArea: React.FC<TextAreaProps> = ({
       placeholder={placeholder}
       disabled={disabled}
       onChange={handleChange}
-      className={`w-full px-3 py-2 box-border rounded-md border border-gray-400 bg-white text-black focus:outline-none focus:ring-1 focus:ring-gray-200 resize-none ${sizeMap[size]}`}
+      className={cn(
+        'w-full px-3 py-2 box-border rounded-md bg-white resize-none focus:outline-none focus:ring-1 focus:ring-gray-200',
+        sizeMap[size],
+        'border',
+        'border-[color:var(--color-gray-40)]',
+        'placeholder-[color:var(--color-gray-40)]',
+        disabled ? 'text-[color:var(--color-gray-40)] bg-gray-10 cursor-not-allowed' : 'text-black',
+      )}
     />
   );
 };
