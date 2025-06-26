9d23c0dc1ff572755f55a97acbad0a1af5d43e32
이상진 <102288397+lsj0202@users.noreply.github.com>
2025-06-26 09:28:43 +0900

    feat: Typography, Tag 컴포넌트 개발 및 스토리북 작성 (#22)
    * feat: tag 컴포넌트 개발 및 스토리북 작성
    
    * feat: typography, tag 컴포넌트 개발 및 스토리북 작성
    
    * chore: typography type 변경
    
    * chore: 리뷰 반영
    
    * chore: 중복 사용된 typeof 컴포넌트 type을 하나로 통일

---
+++ src/components/common/Typography/index.tsx
@@ -0,0 +1,50 @@
+import type { VariantProps } from 'class-variance-authority';
+import { cva } from 'class-variance-authority';
+import type { HTMLAttributes } from 'react';
+import { forwardRef } from 'react';
+
+import { cn } from '@/utils/styleMerge';
+
+const typographyVariants = cva('whitespace-pre-line text-wrap', {
+  variants: {
+    type: {
+      heading1: 'font-bold text-[24px] leading-[145%]',
+      heading2: 'font-bold text-[22px] leading-[145%]',
+      heading3: 'font-bold text-[20px] leading-[145%]',
+      heading4: 'font-bold text-[18px] leading-[155%]',
+      title1: 'font-semibold text-[20px] leading-[155%]',
+      title2: 'font-semibold text-[18px] leading-[155%]',
+      title3: 'font-semibold text-[16px] leading-[155%]',
+      title4: 'font-semibold text-[15px] leading-[155%]',
+      title5: 'font-semibold text-[14px] leading-[155%]',
+      body1: 'font-medium text-[17px] leading-[155%]',
+      body2: 'font-medium text-[16px] leading-[155%]',
+      body3: 'font-medium text-[15px] leading-[155%]',
+      body4: 'font-medium text-[14px] leading-[155%]',
+    },
+  },
+  defaultVariants: {
+    type: 'body3',
+  },
+});
+
+export type fontType = VariantProps<typeof typographyVariants>['type'];
+
+type TypographyProps = {
+  children: React.ReactNode;
+} & HTMLAttributes<HTMLSpanElement> &
+  VariantProps<typeof typographyVariants>;
+
+const Typography = forwardRef<HTMLSpanElement, TypographyProps>(({ children, type, ...props }, ref) => {
+  const { className, ...restProps } = props;
+
+  return (
+    <span ref={ref} className={cn(typographyVariants({ type }), className)} {...restProps}>
+      {children}
+    </span>
+  );
+});
+
+Typography.displayName = 'Typography';
+
+export default Typography;
