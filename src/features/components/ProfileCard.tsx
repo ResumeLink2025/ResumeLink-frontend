35f52d778bf3991b6b4e0acbdf2fedcea1420a51
lsj0202 <w1004123@gmail.com>
2025-06-26 13:32:04 +0900

    fix: profileCard 구조 변경 및 스타일 코드 수정

--- src/features/components/ProfileCard.tsx
+++ src/features/components/ProfileCard.tsx
@@ -1,101 +1,85 @@
-'use client';
 import { Heart } from 'lucide-react';
 import Image from 'next/image';
 
-import UseLikeToggle from '@/hooks/UseLikeToggle';
+import { Tag, Typography } from '@/components/common';
+import { cn } from '@/utils/styleMerge';
 
-interface ProfileCardProps {
+export type ProfileType = {
+  id: number;
   imageUrl: string;
   title: string;
-  year: string;
+  categories: string[];
   description: string;
   avatarUrl: string;
   author: string;
-  likes: number;
-}
+  likeCount: number;
+  isLiked?: boolean;
+};
+
+type ProfileCardProps = {
+  onClick?: () => void;
+} & ProfileType;
 
 export default function ProfileCard({
   imageUrl,
   title,
-  year,
+  categories,
   description,
   avatarUrl,
   author,
-  likes,
+  likeCount,
+  isLiked,
+  onClick,
 }: ProfileCardProps) {
-  const { liked, count, toggle } = UseLikeToggle(false, likes);
-
   return (
-    <div
-      className="rounded-2xl overflow-hidden bg-white"
-      style={{ boxShadow: 'var(--shadow-button)', width: 230 }}
-    >
+    <div className="shadow-button min-w-[230px] rounded-[10px] cursor-pointer">
       <Image
         src={imageUrl}
         alt={title}
         width={230}
-        height={180}
-        className="w-full object-cover rounded-t-xl"
-        style={{ height: 180, border: '0.5px solid var(--color-gray-30)', boxSizing: 'border-box' }}
+        height={200}
+        className="w-full h-50 object-cover rounded-t-[10px] box-border"
       />
-      <div className="space-y-2" style={{ width: 230 }}>
-        <div className="px-4 py-2">
-          <h3 className=" text-[var(--color-gray-70)] py-1 px-0 m-0" style={{ fontWeight: 500 }}>
-            {title}
-          </h3>
-
-          <span
-            className="bg-[var(--color-gray-20)] text-[var(--color-gray-60)] rounded py-1 px-1 "
-            style={{ fontSize: 'var(--text-5xs)' }}
-          >
-            {year}년차
-          </span>
-
-          <p
-            className="line-clamp-3 leading-relaxed py-1 px-0 my-1"
-            style={{
-              color: 'var(--color-gray-60)',
-              fontSize: 'var(--text-5xs)',
-            }}
-          >
-            {description}
-          </p>
+      <div className="flex flex-col gap-2 h-36 border-t-1 border-gray-20 py-[6px] px-2 bg-white">
+        <Typography type="title2">{title}</Typography>
+        <div className="flex flex-wrap gap-1">
+          {categories.map((category, idx) => (
+            <Tag key={idx} size="small" styleType="gray">
+              {category}
+            </Tag>
+          ))}
         </div>
-
-        <div
-          className="flex justify-between items-center text-[var(--text-5xs)] pt-2 mt-2 border-[var(--color-gray-20)] px-4 py-3"
-          style={{ boxSizing: 'border-box', borderTop: '0.5px solid var(--color-gray-30) ' }}
-        >
-          <div className="flex items-center gap-2">
-            <Image
-              src={avatarUrl}
-              alt={`${author} `}
-              width={24}
-              height={24}
-              className="rounded-full object-cover"
-              style={{ border: '1px solid var(--color-gray-30)' }}
-            />
-            <span className="text-[var(--color-gray-50)]" style={{ fontSize: 'var(--text-5xs)' }}>
-              by{' '}
-              <strong className="text-[var(--color-gray-60)] fl" style={{ fontSize: 'var(--text-5xs)' }}>
-                @{author}
-              </strong>
-            </span>
+        <Typography type="body4" className="text-gray-50 line-clamp-3">
+          {description}
+        </Typography>
+      </div>
+      <div className="border-t-1 border-gray-20 flex items-center justify-between rounded-b-[10px] py-[6px] px-2">
+        <div className="flex items-center gap-2">
+          <Image
+            src={avatarUrl}
+            alt={author}
+            width={24}
+            height={24}
+            className="rounded-full object-cover border-1 border-gray-30"
+          />
+          <div className="flex gap-1">
+            <Typography type="body3" className="text-gray-40">
+              by
+            </Typography>
+            <Typography type="body3" className="text-gray-70">
+              {author}
+            </Typography>
           </div>
-
-          <button onClick={toggle} className="flex items-center gap-1 focus:outline-none">
-            <Heart
-              size={14}
-              className={`transition-colors ${
-                liked ? 'text-[var(--color-primary)]' : 'text-[var(--color-gray-50)]'
-              }`}
-              fill={liked ? 'var(--color-primary)' : 'none'}
-              cursor="pointer"
-            />
-            <span className="text-[var(--color-gray-60)]" style={{ fontSize: 'var(--text-5xs)' }}>
-              {count}
-            </span>
-          </button>
+        </div>
+        <div onClick={onClick} className="flex items-center gap-1">
+          <Heart
+            size={15}
+            fill={isLiked ? 'var(--color-primary)' : 'none'}
+            className={cn('transition mb-[2px]', isLiked ? 'text-primaryHover' : 'text-gray-50')}
+          />
+          <Typography type="body5" className="text-gray-60">
+            {likeCount}
+          </Typography>
         </div>
       </div>
     </div>
