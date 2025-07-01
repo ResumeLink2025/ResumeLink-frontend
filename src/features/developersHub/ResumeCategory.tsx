2c96bd47ad6ad0350ef09016cc3b75cb0818c0b0
lsj0202 <w1004123@gmail.com>
2025-07-01 09:34:41 +0900

    chore: 카테고리 리스트 타이틀 수정

--- src/features/developersHub/ResumeCategory.tsx
+++ src/features/developersHub/ResumeCategory.tsx
@@ -12,13 +12,13 @@ const ResumeCategory = () => {
     <div className="flex flex-col gap-4">
       <div className="flex justify-between gap-2 max-h-50 overflow-y-auto hide-scrollbar">
         <CategoryList
-          title="개발 직군"
+          title="개발 연차"
           categories={DEVELOP_EXPERIENCE}
           checkedList={experience}
           onClickCategory={onClickExperience}
         />
         <CategoryList
-          title="개발 연차"
+          title="개발 직군"
           categories={DEVELOP_OCCUPATION}
           checkedList={occupation}
           onClickCategory={onClickOccupation}
