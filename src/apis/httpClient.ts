5c3ca3b5cccd509a4ce62800cb4d45727a76e9e2
lsj0202 <w1004123@gmail.com>
2025-07-19 02:04:25 +0900

    chore: 테스트에서 사용하는 코드 주석 처리

--- src/apis/httpClient.ts
+++ src/apis/httpClient.ts
@@ -17,10 +17,10 @@ httpClient.interceptors.request.use(
       config.headers.authorization = `Bearer ${accessToken}`;
     }
 
-    if (config.method?.toLowerCase() === 'get') {
-      config.headers['ngrok-skip-browser-warning'] = '69420';
-      config.headers['Accept'] = 'application/json';
-    }
+    // if (config.method?.toLowerCase() === 'get') {
+    //   config.headers['ngrok-skip-browser-warning'] = '69420';
+    //   config.headers['Accept'] = 'application/json';
+    // }
 
     return config;
   },
