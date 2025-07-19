510d3c70b2b6f1de071a3aacc4387e2d68f59640
lsj0202 <w1004123@gmail.com>
2025-07-19 22:51:05 +0900

    chore: 프로젝트 새성시 프로젝트 상세 페이지로 이동하도록 수정

--- src/hooks/apis/project/useCreateProject.ts
+++ src/hooks/apis/project/useCreateProject.ts
@@ -5,7 +5,11 @@ import type { AxiosError } from 'axios';
 import { post } from '@/apis/httpClient';
 import type { ProjectFormDataType } from '@/features/project/schemas/projectSchema';
 
-const useCreateProject = (options?: UseMutationOptions<unknown, AxiosError, ProjectFormDataType>) => {
+export type createProjectResponseType = { data: { projectNumber: string } };
+
+const useCreateProject = (
+  options?: UseMutationOptions<createProjectResponseType, AxiosError, ProjectFormDataType>,
+) => {
   return useMutation({
     mutationFn: (data: ProjectFormDataType) => post('/api/projects', data),
     ...options,
